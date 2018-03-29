c6f481b414d6b43f3f140079d0a5441644a81ff6
andrew-korneev <heterogeneous@yandex.ru>
2018-03-30 00:34:58 +0500

    XML 'autowire' and 'scope' attributes; Importing sub contexts;

--- src/main/java/impls/robots/RobotMark1.java
+++ src/main/java/impls/robots/RobotMark1.java
@@ -12,7 +12,7 @@ import org.springframework.util.StringUtils;
 public class RobotMark1 implements Robot {
 
     private String name;
-    private int health;
+    private int health = 100;
 
     private Hand hand;
     private Leg leg;
@@ -21,12 +21,35 @@ public class RobotMark1 implements Robot {
     public RobotMark1() {
     }
 
-    public RobotMark1(Hand hand, Leg leg, Head head, int health) {
+    public RobotMark1(Hand hand, Leg leg, Head head) {
         super();
         this.hand = hand;
         this.leg = leg;
         this.head = head;
-        this.health = health;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Leg getLeg() {
+        return leg;
+    }
+
+    public void setLeg(Leg leg) {
+        this.leg = leg;
+    }
+
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
     }
 
     public String getName() {
@@ -54,7 +77,7 @@ public class RobotMark1 implements Robot {
 
     @Override
     public void say() {
-        System.out.println("\nSUP DUDE\n");
+        System.out.println(this + "\nSUP DUDE\n");
     }
 
     @Override
